556ff7a63bd36eb3df4d951063ec62031816d968
Adrian Salgado Lopez <41001.adrian@gmail.com>
2021-02-25 20:12:49 +0000

    Change greet function to random personal fact function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random personal fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFact() {
+  const facts =
+      ['I live in LA', 'Pronouns: he/him/his!', 'I attend CSULA', 'My favorite artist is King Krule'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
+  const fact = facts[Math.floor(Math.random() * facts.length)];
+  console.log(fact);
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factContainer = document.getElementById('greeting-container');
+  factContainer.innerText = greeting;
 }
