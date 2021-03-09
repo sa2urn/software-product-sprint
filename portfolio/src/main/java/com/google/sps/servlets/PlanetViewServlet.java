63aee411ebc7c09ddad8081e4a97064daacf10b3
Adrian Salgado Lopez <41001.adrian@gmail.com>
2021-03-09 00:50:18 +0000

    add ">"

--- portfolio/src/main/java/com/google/sps/servlets/PlanetViewServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PlanetViewServlet.java
@@ -19,11 +19,11 @@ public class PlanetViewServlet extends HttpServlet {
     int planetChosen = (int)(Math.random() * ((planets.length - 1)  - 0 + 1) + 0);
 
     response.setContentType("text/html");
-    response.getWriter().print("<body background=\"/images/space.jpg\" style=\"font-family: Verdana, sans-serif;color:white; text-align:center; background-color: rgb(0, 0, 0, 0.6); background-blend-mode: multiply;\">");
+    response.getWriter().println("<body background=\"/images/space.jpg\" style=\"font-family: Verdana, sans-serif;color:white; text-align:center; background-color: rgb(0, 0, 0, 0.6); background-blend-mode: multiply;\">");
     response.getWriter().println("<h1 style=\"padding-top: 20px; font-size: 50px;\">" + planetNames[planetChosen] + "</h1>");
     response.getWriter().println("<img heigt=5% width= auto src=\"" + planets[planetChosen] + "\" alt=\"Planet image\">");
     response.getWriter().println("<h2 style=\"font-size:25px\">Facts</h2>");
-    response.getWriter().println("<p style=\"font-size:20px\">" + facts[planetChosen] + "</p");
-    response.getWriter().println("<p style=\"font-size:20px\">" + facts2[planetChosen] + "</p");
+    response.getWriter().println("<p style=\"font-size:20px\">" + facts[planetChosen] + "</p>");
+    response.getWriter().println("<p style=\"font-size:20px\">" + facts2[planetChosen] + "</p>");
   }
 }
