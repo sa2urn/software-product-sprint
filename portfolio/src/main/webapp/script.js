47f4d355436b309b2569ef7d3bb72f352b50816f
Adrian Salgado Lopez <41001.adrian@gmail.com>
2021-03-10 02:48:22 +0000

    Function converts response to json. Create switch case method to randomly choose a message from the json data.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,7 +1,38 @@
 async function showGreetings(button) {
   const responseFromServer = await fetch('/greetings');
-  const textFromResponse = await responseFromServer.text();
-  
+  const greetings = await responseFromServer.json();
   const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = textFromResponse;
+
+  switch (Math.floor(Math.random() * 10)) {
+    case 0:
+      greetingContainer.innerText = greetings.greeting0;
+      break;
+    case 1:
+      greetingContainer.innerText = greetings.greeting1;
+      break;
+    case 2:
+      greetingContainer.innerText = greetings.greeting2;
+      break;
+    case 3:
+      greetingContainer.innerText = greetings.greeting3;
+      break;
+    case 4:
+      greetingContainer.innerText = greetings.greeting4;
+      break;
+    case 5:
+      greetingContainer.innerText = greetings.greeting5;
+      break;
+    case 6:
+      greetingContainer.innerText = greetings.greeting6;
+      break;
+    case 7:
+      greetingContainer.innerText = greetings.greeting7;
+      break;
+    case 8:
+      greetingContainer.innerText = greetings.greeting8;
+      break;
+    case 9:
+      greetingContainer.innerText = greetings.greeting9;
+      break;
+    }
 }
