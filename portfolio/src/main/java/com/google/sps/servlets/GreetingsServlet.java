cf2ce613f79e2fdfb73088525ecf9ab2d3efa852
Adrian Salgado Lopez <41001.adrian@gmail.com>
2021-03-10 02:46:54 +0000

    Switch Array to ArrayList, doGet()  returns a json string and create Method that converts ArrayList to a String.

--- portfolio/src/main/java/com/google/sps/servlets/GreetingsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/GreetingsServlet.java
@@ -1,7 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +9,41 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/greetings")
 public class GreetingsServlet extends HttpServlet {
-  private String[] greetings = {"Bonjour", "Hola", "Привет", "你好", "こんにちは", "안녕하세요", "Olá", "مرحبا", "γεια σας", "Hello"};
+  private ArrayList<String> greetings = new ArrayList<String>();
+
+  public GreetingsServlet() {
+    greetings.add("Bonjour");
+    greetings.add("Hola");
+    greetings.add("Привет");
+    greetings.add("你好");
+    greetings.add("こんにちは");
+    greetings.add("안녕하세요");
+    greetings.add("Olá");
+    greetings.add("مرحبا");
+    greetings.add("γεια σας");
+    greetings.add("Hello");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    int greetingChosen = (int) (Math.random() * ((greetings.length - 1) - 0 + 1) + 0);
+    String json = convertToJson();
     
-    response.setContentType("text/html; charset=UTF-8");
-    response.getWriter().println(greetings[greetingChosen]);
+    response.setContentType("application/json; charset=UTF-8");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(){
+    String json = "{";
+    json += "\"size \": " + "\"" + String.valueOf(greetings.size()) + "\", ";
+
+    for(int indx = 0; indx < greetings.size(); indx++){
+        json+= "\"greeting" + indx + "\": ";
+        json+= "\"" + greetings.get(indx) +"\"";
+
+        if(indx != (greetings.size() - 1)) json+= ", ";
+    }
+
+    json+="}";
+    return json;
   }
 }
